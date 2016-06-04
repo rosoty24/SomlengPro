f7f30c5d4b89c1b5cc611d61e46dc9a6d1b9b249
pokoet <lypokoet@gmail.com>
2016-06-04 16:50:34 +0700

    new

--- client/Frontend/Controller/index.js
+++ client/Frontend/Controller/index.js
@@ -1,3 +1,6 @@
 Template.index.helpers({
   singerIndex: () => SingerIndex
 });
+Template.index.helpers({
+  inputAttributes: function () { return { class: 'form-control', placeholder: 'Search singer...' }; }
+});
