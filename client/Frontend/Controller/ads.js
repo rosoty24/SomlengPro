8ae5a79ecad1aa4db79bd1d499ab87a7192b7298
rosoty24 <rosoty24@gmail.com>
2016-06-30 23:07:37 +0700

    new

---
+++ client/Frontend/Controller/ads.js
@@ -0,0 +1,18 @@
+// Template.rightside.redered = function(){
+// 	$.getScript("//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",function(){
+// 		var ads,adsbygoogle;
+// 		ads = '<ins class="adsbygoogle" style="display:inline-block;width:300px;height:600px" data-ad-client="ca-pub-3963205054734109" data-ad-slot="4331288073"></ins>';
+// 		$('#ads-right').html(ads);
+// 		return (adsbygoogle = window.adsbygoogle || []).push({});
+// 	});
+// }
+				// <!-- somleng -->
+				// <ins class="adsbygoogle"
+				//      style="display:inline-block;width:300px;height:600px"
+				//      data-ad-client="ca-pub-3963205054734109"
+				//      data-ad-slot="4331288073"></ins>
+				// <script>
+				// (adsbygoogle = window.adsbygoogle || []).push({});
+				// </script>
+				// <!-- <img src="/ads/sz168-1.gif" width="100%"/> -->
+
