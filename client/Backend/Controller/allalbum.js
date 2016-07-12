da33866c5ddc9f88366f96fb8bb5070e8a80c3b3
rosoty24 <rosoty99@gmail.com>
2016-07-13 00:18:46 +0700

    new

--- client/Backend/Controller/allalbum.js
+++
@@ -1,9 +0,0 @@
-Template.allalbums.events({
-	"click #remove":function(e){
-		e.preventDefault();
-		var id = this._id;
-		if(confirm("Are you sure want to delete this !!!")){
-			Meteor.call("RemoveAlbum",id);
-		}
-	}
-});
--- server/review.js
+++
@@ -1,8 +0,0 @@
-Meteor.methods({
-	AddReview:function(obj){
-		review.insert(obj);
-	},
-	RemoveReview:function(musicid,userid){
-		review.remove({musicId:musicid,userId:userid});
-	}
-})
